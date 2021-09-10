0bd63b973459165daaed7e494326c397d8eeae2a
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-09-10 20:40:37 +0430

    Create Thumbnail for Picture

--- DataAccess/Design Pattern/Repositories/Classes/AboutMeRepository.cs
+++ DataAccess/Design Pattern/Repositories/Classes/AboutMeRepository.cs
@@ -36,7 +36,7 @@ namespace DataAccess.Design_Pattern.Repositories.Classes
 
 
             };
-            if (resume.AvatarName != null)
+            if (resume.AvatarName != null && resume.AvatarName.IsImage())
             {
                 string imagePath = "";
                 about.AvatarName = NameGenerator.GenerateUniqCode() + Path.GetExtension(resume.AvatarName.FileName);
@@ -45,6 +45,11 @@ namespace DataAccess.Design_Pattern.Repositories.Classes
                 {
                     resume.AvatarName.CopyTo(stream);
                 }
+
+                ImageConvertor imgResizer = new ImageConvertor();
+                string thumbPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar/Thumb", about.AvatarName);
+
+                imgResizer.Image_resize(imagePath, thumbPath, 150);
             }
 
             Add(about);
@@ -60,6 +65,11 @@ namespace DataAccess.Design_Pattern.Repositories.Classes
                     File.Delete(deleteimagePath);
                 }
 
+                string deletethumbPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar/Thumb", resume.AvatarName);
+                if (File.Exists(deletethumbPath))
+                {
+                    File.Delete(deletethumbPath);
+                }
             }
 
             Delete(resume);
@@ -84,7 +94,6 @@ namespace DataAccess.Design_Pattern.Repositories.Classes
         {
             if (imgProductUp != null && imgProductUp.IsImage())
             {
-
                 if (resume.AvatarName != "Defult.jpg")
                 {
                     string deleteimagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar", resume.AvatarName);
@@ -92,17 +101,23 @@ namespace DataAccess.Design_Pattern.Repositories.Classes
                     {
                         File.Delete(deleteimagePath);
                     }
+
+                    string deletethumbPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar/Thumb", resume.AvatarName);
+                    if (File.Exists(deletethumbPath))
+                    {
+                        File.Delete(deletethumbPath);
+                    }
                 }
-
-
 
                 resume.AvatarName = NameGenerator.GenerateUniqCode() + Path.GetExtension(imgProductUp.FileName);
                 string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar", resume.AvatarName);
-
                 using (var stream = new FileStream(imagePath, FileMode.Create))
                 {
                     imgProductUp.CopyTo(stream);
                 }
+
+                ImageConvertor imgResizer = new ImageConvertor();
+                string thumbPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/UserAvatar/Thumb", resume.AvatarName);
 
             }
 
